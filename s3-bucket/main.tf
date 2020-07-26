95d3af17dd95b25cdcab3235001ab351c365c2a5
Lars Willrich <larswillrich@gmail.com>
2020-07-26 23:37:35 +0200

    add vars

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-central-1"
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "123980123890982348263487"
+  bucket = "${var.organization}-${var.organizationUnit}-${var.stage}-123980123890982348263487"
   acl    = "private"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "s3 bucket"
+    Environment = var.stage
   }
 }
---
+++ s3-bucket/variable.tf
@@ -0,0 +1,12 @@
+variable "organization" {
+  description = "organization"
+}
+variable "organizationUnit" {
+  description = "organizationUnit"
+}
+variable "account" {
+  description = "account"
+}
+variable "stage" {
+  description = "stage"
+}
